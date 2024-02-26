2091da33d7fe6adf271e15eeae06a5ab2a14d1ce
kiran <kiran.kumar@voltuswave.com>
2024-02-26 17:56:03 +0530

    push

--- Onboarding/usersSignupToAccountAdmin.js
+++ Onboarding/usersSignupToAccountAdmin.js
@@ -497,7 +497,7 @@ module.exports = userSignupToAccountAdmin = `
                                                                                 src={{platformLogo}}
                                                                                 alt
                                                                                 style="display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;
-                                                                                width:300px; height: 90px;object-fit: contain;"
+                                                                                width:250px; height: 90px;object-fit: contain;"
                                                                             />
                                                                         </a>
                                                                         </div>
